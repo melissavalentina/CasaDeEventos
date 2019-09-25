e04155c664af7fa13fa5f1fca4b48098005d5841
melissavalentina <55805924+melissavalentina@users.noreply.github.com>
2019-09-25 12:56:36 -0500

    project 1.0

---
+++ src/casadeeventos/CasaDeEventos.java
@@ -0,0 +1,20 @@
+
+package casadeeventos;
+
+import java.util.Date;
+
+
+
+
+
+public class CasaDeEventos {
+    
+private int numeroDeMesas;
+private int numeroDeSillas;
+private Date fechaDelEvento;
+private String nombreDelChef;
+private boolean auditorio;
+
+
+}
+
